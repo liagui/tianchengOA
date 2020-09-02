98d0bcd367c8832b7217bb6d1e104eb9842414f7
Suzhenwen <2663930382@qq.com>
2020-09-02 17:06:00 +0800

    测试路由

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -6,6 +6,7 @@ use App\Models\Pay_order_inside;
 class OrderController extends Controller {
     //订单总览
     public function orderList(){
+        return $this->response('123456789');
         $list = Pay_order_inside::orderList(self::$accept_data);
         return $this->response($list);
     }
