aa775a63e54ef61ecc383ccd9940333854847c93
Suzhenwen <2663930382@qq.com>
2020-09-03 14:33:22 +0800

    订单总览

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -6,8 +6,12 @@ use App\Models\Pay_order_inside;
 class OrderController extends Controller {
     //订单总览
     public function orderList(){
-        return $this->response('123456789');
         $list = Pay_order_inside::orderList(self::$accept_data);
-        return $this->response($list);
+        return response()->json($list);
+    }
+    //手动报单
+    public function handOrder(){
+        $list = Pay_order_inside::handOrder(self::$accept_data);
+        return response()->json($list);
     }
 }
