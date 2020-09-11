56b79a078fb9dbffc1f9147ab7151dbaf01a8aee
liagui <315142971@qq.com>
2020-09-11 20:36:14 +0800

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,38 +15,41 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        // $response = $next($request);
-        // $IlluminateResponse = 'Illuminate\Http\Response';
-        // $SymfonyResopnse = 'Symfony\Component\HttpFoundation\Response';
-        // $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-        // $headers = [
-        //     'Access-Control-Allow-Origin' => $origin,
-        //     'Access-Control-Allow-Credentials' => 'true',
-        //     'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, PATCH, DELETE',
-        //     'Access-Control-Allow-Headers' => 'token,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Authorization , Access-Control-Request-Headers, X-CSRF-TOKEN'
-        // ];
- 
-        // if ($response instanceof $IlluminateResponse) {
-        //     foreach ($headers as $key => $value) {
-        //         $response->header($key, $value);
-        //     }
-        //     return $response;
-        // }
- 
-        // if ($response instanceof $SymfonyResopnse) {
-        //     foreach ($headers as $key => $value) {
-        //         $response->headers->set($key, $value);
-        //     }
-        //     return $response;
-        // }
-        // return $response;
+        $response = $next($request);
+        $IlluminateResponse = 'Illuminate\Http\Response';
+        $SymfonyResopnse = 'Symfony\Component\HttpFoundation\Response';
+        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
+        $headers = [
+            'Access-Control-Max-Age'=>'1728000',
 
-        return $next($request)
-        ->header('Access-Control-Max-Age', '1728000')
-        ->header('Access-Control-Allow-Credentials', 'false')
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Headers', 'Authorization')
-        ->header('Access-Control-Expose-Headers', 'Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, X-Requested-By, Access-Control-Allow-Methods')
-        ->header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+            'Access-Control-Allow-Origin' => $origin,
+            'Access-Control-Allow-Credentials' => 'false', //true;
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, PATCH, DELETE',
+            'Access-Control-Expose-Headers' => 'Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, X-Requested-By, Access-Control-Allow-Methods',
+            'Access-Control-Allow-Headers'=>'Authorization'
+        ];
+ 
+        if ($response instanceof $IlluminateResponse) {
+            foreach ($headers as $key => $value) {
+                $response->header($key, $value);
+            }
+            return $response;
+        }
+ 
+        if ($response instanceof $SymfonyResopnse) {
+            foreach ($headers as $key => $value) {
+                $response->headers->set($key, $value);
+            }
+            return $response;
+        }
+        return $response;
+
+        // return $next($request)
+        // ->header('Access-Control-Max-Age', '1728000')
+        // ->header('Access-Control-Allow-Credentials', 'false')
+        // ->header('Access-Control-Allow-Origin', '*')
+        // ->header('Access-Control-Allow-Headers', 'Authorization')
+        // ->header('Access-Control-Expose-Headers', 'Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, X-Requested-By, Access-Control-Allow-Methods')
+        // ->header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     }
 }
