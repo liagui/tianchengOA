22216bff667fd42f8a0c4c78726c7a7cea78eb9a
liagui <315142971@qq.com>
2020-09-11 19:18:28 +0800

    add 学员资料导出

---
+++ app/Exports/StudentDatumExport.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Exports;
+use App\Models\School;
+use App\Models\AdminLog;
+use App\Models\StudnetDatum;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+class StudentDatumExport implements FromCollection, WithHeadings {
+
+    protected $where;
+    public function __construct($invoices){
+        $this->where = $invoices;
+    }
+    public function collection() {
+        $data = $this->where;
+        $arr =School::select('id')->get();
+
+        return $arr;
+
+        
+    }
+
+    public function headings(): array
+    {
+       
+        return [
+            '所属分校',
+            // '报考月份',
+            // '报考地区',
+            // '备考地区',
+            // '学员姓名',
+            // '学员性别',
+            // '学员身份证',
+        ];
+    }
+}
