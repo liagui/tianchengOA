c04835ecb13f2b46f749030eaaf332926d7cad26
Suzhenwen <2663930382@qq.com>
2020-09-11 17:03:47 +0800

    订单

--- app/Models/Pay_order_inside.php
+++ app/Models/Pay_order_inside.php
@@ -472,6 +472,10 @@ class Pay_order_inside extends Model
         $admin = isset(AdminLog::getAdminInfo()->admin_user) ? AdminLog::getAdminInfo()->admin_user : [];
         $order = self::where(['id'=>$data['id']])->first();
         unset($data['/admin/order/notarizeOrder']);
+        if(empty($data['education_id'])){
+            unset($data['education_id']);
+            unset($data['major_id']);
+        }
         if($data['confirm_status'] == 1){
             $data['comfirm_time'] = date('Y-m-d H:i:s');
             $data['have_user_id'] = $admin['id'];
