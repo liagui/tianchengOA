119a3051bff84d6c49f810365696e6cf3227361c
liagui <315142971@qq.com>
2020-09-17 10:39:35 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -261,7 +261,7 @@ $router->group(['prefix' => 'admin' , 'namespace' => 'Admin','middleware'=> ['jw
         $router->post('getRegionList', 'StudentDatumController@getRegionList');  //获取发起人信息
         $router->post('getDatumCount', 'StudentDatumController@getDatumCount');  //获取资料数量
         //导出业绩
-        $router->get('exportTeacherPerformance', 'TeacherController@exportTeacherPerformance');
+        $router->post('exportTeacherPerformance', 'TeacherController@exportTeacherPerformance');
     });
 
 
