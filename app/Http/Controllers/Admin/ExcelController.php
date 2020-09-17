8e963416f8e2033ac147fdabb99ec4142b7f1de5
Suzhenwen <2663930382@qq.com>
2020-09-17 15:42:54 +0800

    jwt

--- app/Http/Controllers/Admin/ExcelController.php
+++ app/Http/Controllers/Admin/ExcelController.php
@@ -10,8 +10,7 @@ class ExcelController extends Controller{
 	    $jwt = new JWTRoleAuth();
 	    $data = self::$accept_data;
 	    $token = $jwt->handle($data['token'],'/admin/doExcelDatum');
-        print_r($token);die;
 //		$data['schoolids'] = $this->underlingLook(isset(AdminLog::getAdminInfo()->admin_user->school_id) ? AdminLog::getAdminInfo()->admin_user->school_id : 0);
-//        return Excel::download(new \App\Exports\StudentDatumExport($data,$user), '学员报名资料.xlsx');
+        return Excel::download(new \App\Exports\StudentDatumExport($data,$user), '学员报名资料.xlsx');
 	}
 }
