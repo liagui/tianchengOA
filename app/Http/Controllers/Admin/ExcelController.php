138e0c3565f38d86007f7d63a1a8eae8a951b574
Suzhenwen <2663930382@qq.com>
2020-09-17 15:35:52 +0800

    jwt

--- app/Http/Controllers/Admin/ExcelController.php
+++ app/Http/Controllers/Admin/ExcelController.php
@@ -3,19 +3,20 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Middleware\JWTRoleAuth;
 use App\Models\AdminLog;
+use http\Header;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
-class ExcelController extends BaseMiddleware{
+class ExcelController extends Controller{
 	public function doExcelDatum(){
-
-        $data = $_REQUEST;
-            // 解析token角色
-        $token_role = $this->auth->parseToken()->getClaim('role');
-        print_r($token_role);die;
+	    include_once("./app/Http/Middleware/JWTRoleAuth.php");
+	    $jwt = new JWTRoleAuth();
+	    $data = self::$accept_data;
+	    $token = $jwt->handle($data['token'],'/admin/doExcelDatum');
+        print_r($token);die;
 //		$data['schoolids'] = $this->underlingLook(isset(AdminLog::getAdminInfo()->admin_user->school_id) ? AdminLog::getAdminInfo()->admin_user->school_id : 0);
 //        return Excel::download(new \App\Exports\StudentDatumExport($data,$user), '学员报名资料.xlsx');
 	}
