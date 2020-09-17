27699f35de88bcf14a3a1c7e0cc482c514917cf4
liagui <315142971@qq.com>
2020-09-17 17:51:31 +0800

    去掉打印

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -33,7 +33,7 @@ class Region extends Model {
     public static function getRegionList($body){
         $parent_id = isset($body['parent_id']) && !empty($body['parent_id']) && $body['parent_id'] >0 ?$body['parent_id'] :0;
         $data = self::select('id as value','name as label')->where('parent_id',$parent_id)->get()->toArray();
-        print_r($data);die;
+     
         return ['code'=>200,'msg'=>'success','data'=>$data];
     }
 
