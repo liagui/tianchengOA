055f15cc1760682ea6a86ceffe5f56f4971f4168
Suzhenwen <2663930382@qq.com>
2020-09-18 18:03:41 +0800

    支付

--- app/Models/Pay_order_inside.php
+++ app/Models/Pay_order_inside.php
@@ -385,6 +385,11 @@ class Pay_order_inside extends Model
             }else{
                 //如果有 判断班主任id是否等于或大于最后一个数，从第一个开始排 否者数组取下一个
                 $len = count($classlead);
+                print_r($classlead[$len-1]);
+                echo $len.'-----------------';
+                echo $leadid;
+                $have_user_id = $classlead[0]['id'];
+                echo $have_user_id;die;
                 if($classlead[$len-1] == $leadid){
                     print_r($classlead[$len-1]);
                     echo $leadid;
