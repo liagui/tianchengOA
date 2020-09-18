27a859e677967d380bc808e01918525bf2a162b9
liagui <315142971@qq.com>
2020-09-18 22:31:36 +0800

    test

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -81,7 +81,7 @@ class  Channel extends Model {
     	$create_name = isset(AdminLog::getAdminInfo()->admin_user->real_name) ? AdminLog::getAdminInfo()->admin_user->real_name : '';
     	$count = self::where('channel_name','=',$body['channel_name'])->where(['is_forbid'=>0,'is_del'=>0])->count();
         DB::beginTransaction();
-    	if($count >0){
+    	if($count<=0){
             $insert['channel_name'] = $body['channel_name'];
     	    $insert['channel_type'] = $body['channel_type'];
     		$insert['create_time'] = date('Y-m-d H:i:s');
