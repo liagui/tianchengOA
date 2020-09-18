5a20f441f0454a728257b3385be690ae17a689a8
Suzhenwen <2663930382@qq.com>
2020-09-18 17:53:06 +0800

    支付

--- app/Models/Pay_order_inside.php
+++ app/Models/Pay_order_inside.php
@@ -373,6 +373,38 @@ class Pay_order_inside extends Model
          * return  array
          */
     public static function awaitOrder($data,$schoolarr){
+
+        $classlead = Admin::where(['is_del'=>1,'is_forbid'=>1,'status'=>1,'is_use'=>1])->get()->toArray();
+        if(!empty($classlead)){
+            //上次值班的班主任id
+            $leadid = Redis::get('classlead');
+            if(empty($leadid)){
+                //如果没有 就从第一个开始
+                $data['have_user_id'] = $classlead[0]['id'];
+                Redis::set('classlead' , $classlead[0]['id']);
+            }else{
+                //如果有 判断班主任id是否等于或大于最后一个数，从第一个开始排 否者数组取下一个
+                $len = count($classlead,1);
+                if($classlead[$len-1] <= $leadid){
+                    $data['have_user_id'] = $classlead[0]['id'];
+                    Redis::set('classlead' , $classlead[0]['id']);
+                }else{
+                    foreach ($classlead as $k => $v){
+                        if($v['id'] > $leadid){
+                            $data['have_user_id'] = $v['id'];
+                            Redis::set('classlead' , $v['id']);
+                            break;
+                        }
+                    }
+                }
+            }
+        }else{
+            $data['have_user_id'] = 0;
+        }
+        echo $data['have_user_id'];die;
+
+
+
         $where['del_flag'] = 0;  //未删除
         $where['confirm_status'] = 0;  //未确认
         //科目id&学科id
@@ -554,33 +586,33 @@ class Pay_order_inside extends Model
             $data['have_user_name'] = $admin['username'];
             //确认订单  排课
             //值班班主任 排课
-//            $classlead = Admin::where(['is_del'=>1,'is_forbid'=>1,'status'=>1,'is_use'=>1])->get()->toArray();
-//            if(!empty($classlead)){
-//                //上次值班的班主任id
-//                $leadid = Redis::get('classlead');
-//                if(empty($leadid)){
-//                    //如果没有 就从第一个开始
-//                    $data['have_user_id'] = $classlead[0]['id'];
-//                    Redis::set('classlead' , $classlead[0]['id']);
-//                }else{
-//                    //如果有 判断班主任id是否等于或大于最后一个数，从第一个开始排 否者数组取下一个
-//                    $len = count($classlead,1);
-//                    if($classlead[$len-1] <= $leadid){
-//                        $data['have_user_id'] = $classlead[0]['id'];
-//                        Redis::set('classlead' , $classlead[0]['id']);
-//                    }else{
-//                        foreach ($classlead as $k => $v){
-//                            if($v['id'] > $leadid){
-//                                $data['have_user_id'] = $v['id'];
-//                                Redis::set('classlead' , $v['id']);
-//                                break;
-//                            }
-//                        }
-//                    }
-//                }
-//            }else{
+            $classlead = Admin::where(['is_del'=>1,'is_forbid'=>1,'status'=>1,'is_use'=>1])->get()->toArray();
+            if(!empty($classlead)){
+                //上次值班的班主任id
+                $leadid = Redis::get('classlead');
+                if(empty($leadid)){
+                    //如果没有 就从第一个开始
+                    $data['have_user_id'] = $classlead[0]['id'];
+                    Redis::set('classlead' , $classlead[0]['id']);
+                }else{
+                    //如果有 判断班主任id是否等于或大于最后一个数，从第一个开始排 否者数组取下一个
+                    $len = count($classlead,1);
+                    if($classlead[$len-1] <= $leadid){
+                        $data['have_user_id'] = $classlead[0]['id'];
+                        Redis::set('classlead' , $classlead[0]['id']);
+                    }else{
+                        foreach ($classlead as $k => $v){
+                            if($v['id'] > $leadid){
+                                $data['have_user_id'] = $v['id'];
+                                Redis::set('classlead' , $v['id']);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }else{
                 $data['have_user_id'] = 0;
-//            }
+            }
             //计算成本
             //  after_tax_amount  税后金额
             //  return_commission_amount  返佣金额
