a531ee0977b0e27915cf65a26fe4acc8324d1f2c
Suzhenwen <2663930382@qq.com>
2020-09-23 19:50:21 +0800

    扫码配置

--- app/Models/Pay_order_inside.php
+++ app/Models/Pay_order_inside.php
@@ -676,13 +676,13 @@ class Pay_order_inside extends Model
         }
         if($data['confirm_status'] == 1){
             if($data['confirm_order_type'] == 2){
-                if($order['sign_Price'] > $order['pay_price']){
+                if($order['sign_Price'] == $order['pay_price']){
                     return ['code' => 201 , 'msg' => '所填金额大于支付金额'];
                 }
             }
             if($data['confirm_order_type'] == 3){
                 $ppppp = $data['course_Price'] + $data['sign_Price'];
-                if($ppppp > $order['pay_price']){
+                if($ppppp == $order['pay_price']){
                     return ['code' => 201 , 'msg' => '所填金额大于支付金额'];
                 }
             }
