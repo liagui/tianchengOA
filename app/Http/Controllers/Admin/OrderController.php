e907cdea416df19743e2e022f7bb74898a611d20
liagui <315142971@qq.com>
2020-11-06 17:59:42 +0800

    接入银联

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -10,7 +10,7 @@ use App\Models\PaySet;
 use App\Models\Refund_order;
 use App\Tools\AlipayFactory;
 use App\Tools\Hfpos\qrcp_E1103;
-use App\Tools\Hfpos\qrcp_E1103;
+use App\Tools\Yl\YinpayFactory;
 use App\Tools\QRcode;
 use Maatwebsite\Excel\Facades\Excel;
 
