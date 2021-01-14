ce22a085b6eeaff1063b940814fe4ec05245eb99
Suzhenwen <2663930382@qq.com>
2021-01-14 10:16:48 +0800

    分校业绩导出

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -23,7 +23,7 @@ class OrderController extends Controller {
     }
     //订单导出
     public function orderListExceil(){
-        $schoolarr = $this->underlingLook(1);
+        $schoolarr = $this->underlingLook($_GET['adminschool_id']);
         return Excel::download(new \App\Exports\OrderExport(self::$accept_data,$schoolarr['data']), '订单总览.xlsx');
     }
     //手动报单
