71d947f8a2e4caa90b505d57356e9be88bb7b3b4
张泽坤 <31497246+zzk3279372968@users.noreply.github.com>
2021-01-26 22:17:40 +0800

    财务

--- app/Models/Pay_order_inside.php
+++ app/Models/Pay_order_inside.php
@@ -4103,7 +4103,7 @@ class Pay_order_inside extends Model
                        //三级分校的二级抽离金额=三级分校的二级抽比例*实际到款
                        //三级分校的实际返佣=三级分校的返佣金额-三级分校的保证金-三级分校退费*三级分校返佣比例
                        $first_out_of_amount = $v['one_extraction_ratio'] && !empty($v['one_extraction_ratio']) ? $v['one_extraction_ratio'] : '';
-                       $first_out_of_money = $v['first_out_of_amount'] && !empty($v['first_out_of_amount']) ? $v['first_out_of_amount'] : '';
+                       $first_out_of_money = self::where(['school_id'=>$v['school_id'],'pay_status'=>1,'confirm_status'=>2])->sum('first_out_of_amount');
 
                        //二级抽离比例
                        $second_out_of_amount = $v['two_extraction_ratio'] && !empty($v['two_extraction_ratio']) ? $v['two_extraction_ratio'] : '';
