35b4442bbf3e13092c8e189ce9ff306b0b0d556f
张泽坤 <31497246+zzk3279372968@users.noreply.github.com>
2021-01-26 21:49:51 +0800

    333

--- app/Models/Pay_order_inside.php
+++ app/Models/Pay_order_inside.php
@@ -4081,7 +4081,7 @@ class Pay_order_inside extends Model
                        if(!empty($three_school_id)){
                            foreach($three_school_id as $onek=>$onev){
                             $twoprice = self::where(['school_id'=>$onev['id'],'pay_status'=>1,'confirm_status'=>2])->sum('second_out_of_amount');
-                            $twoschoolprice = $twoprice * $onev['two_extraction_ratio'];
+                            $twoschoolprice = $twoprice * ($onev['two_extraction_ratio']/100);
                             $agent_margin = $agent_margin + $twoschoolprice;
                            }
                        }
