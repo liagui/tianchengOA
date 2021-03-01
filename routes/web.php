05e47e448fc178f1890fedad4cc5ceb59009f2e1
suzhenwen0827 <2663930382@qq.com>
2021-03-01 10:36:28 +0800

    退费修改

--- routes/web.php
+++ routes/web.php
@@ -97,6 +97,7 @@ $router->group(['prefix' => 'admin' , 'namespace' => 'Admin','middleware'=> ['jw
         $router->post('remitOrder', 'OrderController@remitOrder');//修改打款状态
         $router->post('relevanceOrder', 'OrderController@relevanceOrder');//关联订单列表
         $router->post('relevanceVoucher', 'OrderController@relevanceVoucher');//关联支付凭证列表
+        $router->post('addremark', 'OrderController@addremark');//添加备注
         //核对订单
         $router->post('auditOrder', 'OrderController@auditOrder');//核对款订单列表
         $router->post('offlinepay', 'OrderController@offlinepay');//核对款根据类型查账户
