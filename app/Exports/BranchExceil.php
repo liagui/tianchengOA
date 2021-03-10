09f2820a3316e9a6e44a5483493a8d3504aca577
suzhenwen0827 <2663930382@qq.com>
2021-03-10 16:19:15 +0800

    分校业绩

--- app/Exports/BranchExceil.php
+++ app/Exports/BranchExceil.php
@@ -33,7 +33,7 @@ class BranchExceil implements FromCollection, WithHeadings{
         $array = [];
         //学校id
         $school_id = [];
-        if (isset($body['school_id'])) {
+        if (isset($data['school_id'])) {
 //            $school_id[0]=$body['school_id'];
             $school_id = School::select('id')->where('school_name', 'like', '%' . $body['school_name'] . '%')->where('is_del', 0)->get();
         }
