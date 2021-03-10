2000de89a65313794ab71c29428fa2d1a385304c
suzhenwen0827 <2663930382@qq.com>
2021-03-10 14:38:36 +0800

    导出增加学校名称

--- app/Exports/BranchExceil.php
+++ app/Exports/BranchExceil.php
@@ -32,13 +32,10 @@ class BranchExceil implements FromCollection, WithHeadings{
         //新数组赋值
         $array = [];
         //学校id
-//        $school_id = [];
-//        if (isset($body['school_name'])) {
+        $school_id = [];
+        if (isset($body['school_id'])) {
+            $school_id[0]=$body['school_id'];
 //            $school_id = School::select('id')->where('school_name', 'like', '%' . $body['school_name'] . '%')->where('is_del', 0)->get();
-//        }
-        $school_id=[];
-        if(isset($body['school_id'])){
-            $school_id[] = $body['school_id'];
         }
 
         if (!empty($body['search_time'])) {
